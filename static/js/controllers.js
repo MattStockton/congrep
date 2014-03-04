a8bbb5c863215b7a0c744d314760128c7abb5642
Matt Stockton <mattstockton@gmail.com>
2014-03-03 23:54:04 -0600

    Fixed spaces / tabs issue

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -10,20 +10,20 @@ var congress_detail_ctrl = function ($scope, $http, $window, $q, $location, $rou
     $scope.run_search = function(bioguide_id){
         congress_service.get_legislator_by_bioguide_id(bioguide_id).then(
             function(legislator){
-        		$scope.legislator = legislator;
-        		
+                $scope.legislator = legislator;
+                
                 $scope.get_top_phrases(bioguide_id);    
                 
                 congress_service.get_entity_id_from_bioguide_id(bioguide_id).then(
-            		function(entity_id){
-            			$scope.legislator.entity_id = entity_id;
-            			
-            			$scope.get_top_donors(entity_id);
+                    function(entity_id){
+                        $scope.legislator.entity_id = entity_id;
+                        
+                        $scope.get_top_donors(entity_id);
                         $scope.get_top_contributing_industries(entity_id);
                         $scope.get_top_contributing_sectors();
                         $scope.get_contribution_breakdown();
                         $scope.get_independent_expenditures();
-            		}
+                    }
                 );
             }
         );
@@ -38,8 +38,8 @@ var congress_detail_ctrl = function ($scope, $http, $window, $q, $location, $rou
     }
     
     $scope.get_top_phrases = function(bioguide_id){     
-    	// Need to use jQuery JSONP because of this: https://github.com/angular/angular.js/issues/1551
-    	$.getJSON('http://capitolwords.org/api/1/phrases.json?entity_type=legislator&n=3&apikey=' + sunlight_api_key + '&entity_value=' + bioguide_id + '&callback=?',
+        // Need to use jQuery JSONP because of this: https://github.com/angular/angular.js/issues/1551
+        $.getJSON('http://capitolwords.org/api/1/phrases.json?entity_type=legislator&n=3&apikey=' + sunlight_api_key + '&entity_value=' + bioguide_id + '&callback=?',
             function(data){
                $scope.top_phrases = data;
             }
@@ -49,7 +49,7 @@ var congress_detail_ctrl = function ($scope, $http, $window, $q, $location, $rou
     $scope.get_top_contributing_industries = function(entity_id){     
         congress_service.get_top_contributing_industries_by_entity_id(entity_id).then(
                 function(data){
-                	$scope.top_contributing_industries = data;
+                    $scope.top_contributing_industries = data;
                 }
         );  
     };
@@ -80,41 +80,41 @@ var congress_detail_ctrl = function ($scope, $http, $window, $q, $location, $rou
     };
     
     $scope.get_sector_name = function(sector){
-    	return congress_service.get_sector(sector);
+        return congress_service.get_sector(sector);
     }
 
     $scope.run_search($routeParams.bioguide_id);
 };
 
 var congress_search_ctrl = function ($scope, $http, $window, $q, $location, congress_service) {
-	$scope.congress_search_text = "";
-	$scope.congress_search_error = undefined;
-	
-	$scope.search = function(){
-		var split_name = $scope.congress_search_text.split(" ");
-		
-		var query = "";
-		if(split_name.length == 2){
-			query = "first_name=" + split_name[0] + "&last_name=" + split_name[1];
-		} else {
-			query = "query=" + $scope.congress_search_text;
-		}
-		
+    $scope.congress_search_text = "";
+    $scope.congress_search_error = undefined;
+    
+    $scope.search = function(){
+        var split_name = $scope.congress_search_text.split(" ");
+        
+        var query = "";
+        if(split_name.length == 2){
+            query = "first_name=" + split_name[0] + "&last_name=" + split_name[1];
+        } else {
+            query = "query=" + $scope.congress_search_text;
+        }
+        
         $http.get(sunlight_root + '/legislators?' + query + '&apikey=' + sunlight_api_key).then(
             function(result){
-            	if(result && result.data && result.data.results && result.data.results.length > 0){
-            		if(result.data.results.length > 1){
-            			$scope.congress_search_error = "More than one result found. Please be more specific";
-            		} else {
-            			var bioguide_id = result.data.results[0].bioguide_id;
-            			$location.path('congress/' + bioguide_id);
-            		}
-            	} else {
-            		$scope.congress_search_error = "No results found";
-            	}
+                if(result && result.data && result.data.results && result.data.results.length > 0){
+                    if(result.data.results.length > 1){
+                        $scope.congress_search_error = "More than one result found. Please be more specific";
+                    } else {
+                        var bioguide_id = result.data.results[0].bioguide_id;
+                        $location.path('congress/' + bioguide_id);
+                    }
+                } else {
+                    $scope.congress_search_error = "No results found";
+                }
             }
         );
-	};
+    };
 }
 
 congress_app.controller('congress_detail_ctrl', ['$scope', '$http', '$window', '$q', '$location', '$routeParams', 'congress_service', congress_detail_ctrl]);
--- static/js/services.js
+++ static/js/services.js
@@ -2,83 +2,83 @@
 var congress_service = angular.module('congress_service', []);
 
 congress_service.service('congress_service', function($http, $q) {
-	
+    
     var SECTOR_LOOKUP = {
-        	"A" : "Agribusiness",
-        	"B" : "Communications/Electronics",
-        	"C" : "Construction",
-        	"D" : "Defense",
-        	"E" : "Energy/Natural Resources",
-        	"F" : "Finance/Insurance/Real Estate",
-        	"H" : "Health",
-        	"K" : "Lawyers and Lobbyists",
-        	"M" : "Transportation",
-        	"N" : "Misc. Business",
-        	"Q" : "Ideology/Single Issue",
-        	"P" : "Labor",
-        	"W" : "Other",
-        	"Y" : "Unknown",
-        	"Z" : "Administrative Use"
+            "A" : "Agribusiness",
+            "B" : "Communications/Electronics",
+            "C" : "Construction",
+            "D" : "Defense",
+            "E" : "Energy/Natural Resources",
+            "F" : "Finance/Insurance/Real Estate",
+            "H" : "Health",
+            "K" : "Lawyers and Lobbyists",
+            "M" : "Transportation",
+            "N" : "Misc. Business",
+            "Q" : "Ideology/Single Issue",
+            "P" : "Labor",
+            "W" : "Other",
+            "Y" : "Unknown",
+            "Z" : "Administrative Use"
     };
     var TRANSPARENCY_DATA_ROOT_URI = 'http://transparencydata.org/api/1.0/';
-	var SUNLIGHT_ROOT_URI = 'https://congress.api.sunlightfoundation.com/';
-	
+    var SUNLIGHT_ROOT_URI = 'https://congress.api.sunlightfoundation.com/';
+    
     this.get_sector = function(sector_id) {
         return SECTOR_LOOKUP[sector_id];
     };
     
     this.get_entity_id_from_bioguide_id = function(bioguide_id){
-    	var deferred = $q.defer();
-    	
+        var deferred = $q.defer();
+        
         $http.jsonp(TRANSPARENCY_DATA_ROOT_URI + 'entities/id_lookup.json?apikey=' + sunlight_api_key + '&bioguide_id=' + bioguide_id + '&callback=JSON_CALLBACK').then(
                 function(data){
                     deferred.resolve(data.data[0].id);
                 },
-        		function(fail_reason){
-                	deferred.reject(fail_reason);
-        		}
+                function(fail_reason){
+                    deferred.reject(fail_reason);
+                }
         );
         return deferred.promise;
     };
 
     this.get_legislator_by_bioguide_id = function(bioguide_id){
-    	var deferred = $q.defer();
-    	
+        var deferred = $q.defer();
+        
         $http.get(SUNLIGHT_ROOT_URI + 'legislators?apikey=' + sunlight_api_key + '&bioguide_id=' + bioguide_id).then(
-        		function(data){
+                function(data){
                     deferred.resolve(data.data.results[0]);
                 },
-        		function(fail_reason){
-                	deferred.reject(fail_reason);
-        		}
+                function(fail_reason){
+                    deferred.reject(fail_reason);
+                }
         );
         return deferred.promise;
     };
     
     this.get_top_donors_by_entity_id = function(entity_id){
-    	var deferred = $q.defer();
+        var deferred = $q.defer();
 
         $http.jsonp(TRANSPARENCY_DATA_ROOT_URI + 'aggregates/pol/' + entity_id + '/contributors.json?&apikey=' + sunlight_api_key + '&callback=JSON_CALLBACK').then(
-        		function(data){
+                function(data){
                     deferred.resolve(data.data);
                 },
-        		function(fail_reason){
-                	deferred.reject(fail_reason);
-        		}
+                function(fail_reason){
+                    deferred.reject(fail_reason);
+                }
         );
         return deferred.promise;
     };    
 
     this.get_top_contributing_industries_by_entity_id = function(entity_id){
-    	var deferred = $q.defer();
+        var deferred = $q.defer();
 
         $http.jsonp(TRANSPARENCY_DATA_ROOT_URI + 'aggregates/pol/' + entity_id + '/contributors/industries.json?&apikey=' + sunlight_api_key + '&callback=JSON_CALLBACK').then(
-        		function(data){
+                function(data){
                     deferred.resolve(data.data);
                 },
-        		function(fail_reason){
-                	deferred.reject(fail_reason);
-        		}
+                function(fail_reason){
+                    deferred.reject(fail_reason);
+                }
         );
         return deferred.promise;
     };      
